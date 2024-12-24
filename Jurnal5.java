be97444626981a130d032e176eeb49306d9930b8
SetiadiAgusnawan <setiadi.a130905@gmail.com>
2024-12-24 11:56:23 +0700

    asses

---
+++ Jurnal5.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Jurnal1 {
+
+    public static void main(final String[] args) {
+        // Kerjakan soalnya di sini
+        scanner Scanner = new Scanner(System.in);
+    }
+}
